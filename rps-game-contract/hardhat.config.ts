60efe39065efdd341e74e79d1eec17ae4e1e6b63
rtvcl // wira <ardefishel@RTVCL---Macbook-Pro-M4.local>
2025-09-27 16:43:31 +0700

    feat: add RPSGame contract with game logic and NFT integration

--- rps-game-contract/hardhat.config.ts
+++ rps-game-contract/hardhat.config.ts
@@ -1,7 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import type { HardhatUserConfig } from "hardhat/config";
 
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
-import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   plugins: [hardhatToolboxViemPlugin],

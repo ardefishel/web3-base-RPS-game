8ca2740a69e3decccf67e084c7b3f216d8d4fe7a
rtvcl // wira <ardefishel@RTVCL---Macbook-Pro-M4.local>
2025-09-27 16:00:09 +0700

    initial: hardhat with base network

---
+++ rps-game-contract/hardhat.config.ts
@@ -0,0 +1,45 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import { configVariable } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxViemPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    baseSepolia: {
+      type: "http",
+      chainType: "op",
+      url: process.env.BASE_SEPOLIA_RPC_URL || "",
+      accounts: [process.env.WALLET_KEY || ""],
+    },
+    baseMainnet: {
+      type: "http",
+      chainType: "op",
+      url: process.env.BASE_MAINNET_RPC_URL || "",
+      accounts: [process.env.WALLET_KEY || ""],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: process.env.ETHERSCAN_API_KEY
+    }
+  }
+};
+
+export default config;
